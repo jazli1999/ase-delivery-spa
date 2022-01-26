e1a4d2d1bf57a5789927f2ce82e42232ff8ec981
Yanjie Li <yanjie.li@tum.de>
2022-01-26 23:13:23 +0100

    Debug login with backend

--- src/components/LoginPage/LoginButton.js
+++ src/components/LoginPage/LoginButton.js
@@ -1,16 +1,15 @@
 import { Button } from 'antd';
 import axios from 'axios';
-// import { useDispatch } from 'react-redux';
-// import { setUser } from './loginSlice';
+import { useDispatch } from 'react-redux';
+import { setUser } from './loginSlice';
 import { api_url } from '../Common/url';
 
 export default function LoginButton(props) {
-    // const dispatch = useDispatch();
+    const dispatch = useDispatch();
     // let user;
 
     let submit = function () {
         let base = Buffer.from(`${props.username}:${props.password}`).toString('base64');
-        console.log(base);
         let csrfToken;
         axios.get(`${api_url}:9091/api/auth/csrf`, {withCredentials: true})
             .then(response => {
@@ -18,7 +17,6 @@ export default function LoginButton(props) {
                     csrfToken = document.cookie.split('; ')
                         .find(row => row.startsWith('XSRF-TOKEN='))
                         .split('=')[1];
-                    console.log(csrfToken);
                     return axios({
                         method: 'POST',
                         url: `${api_url}:9091/api/auth`,
@@ -29,7 +27,23 @@ export default function LoginButton(props) {
                         }
                     });
                 }
-        }).then(response => {console.log(response.status)});
+        }).then(response => {
+            if (response.status === 200) {
+                csrfToken = document.cookie.split('; ')
+                .find(row => row.startsWith('XSRF-TOKEN='))
+                .split('=')[1];
+                return axios({
+                    method: 'GET',
+                    url: `${api_url}:9091/api/auth/credentials/${props.username}`,
+                    headers: {
+                        'Authorization': `Basic ${base}`,
+                        'X-XSRF-TOKEN': csrfToken
+                    }
+                });
+            }
+        }).then(response => {
+            dispatch(setUser({userRole: response.data.toLowerCase(), uname: props.username, uid: 0}))
+        });
     };
 
     return (
